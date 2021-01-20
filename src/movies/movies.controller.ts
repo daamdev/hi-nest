de3077e02f6340fef566c4d9ad15fde2516cd9a0
gofogo2 <promoted.go@gmail.com>
2021-01-20 17:17:13 +0900

    add validation

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { get } from 'http';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { MoviesService } from './movies.service';
 
 
@@ -14,28 +15,28 @@ export class MoviesController {
     }
 
     @Get('search')
-    search(@Query('year') searchYear:string){
+    search(@Query('year') searchYear:number){
         return  `We are searching for a movie made after:${searchYear}`;
     }
 
     @Get('/:id')
-    getOne(@Param('id') movieId:string){
+    getOne(@Param('id') movieId:number){
+        console.log(typeof movieId);
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData: CreateMovieDto){
      return this.moviesService.create(movieData);
     }
 
     @Delete('/:id')
-    remove(@Param('id') movieId:string){
+    remove(@Param('id') movieId:number){
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch('/:id')
-    patch(@Param('id') movieId:string,@Body() updateData){
-        return {updatedMovie:movieId,
-        ...updateData}
+    patch(@Param('id') movieId:number,@Body() updateData: CreateMovieDto){
+        return this.moviesService.update(movieId,updateData);
     }
 }
